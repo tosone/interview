f1753b8a1e2a78b3afe786444b3bd0356c7f0a99
Tosone <i@tosone.cn>
2020-10-23 14:26:53 +0800

    Format key point.

---
+++ exactcover/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func tester(input [9]byte) bool {
+	return false
+}
+
+func main() {
+	var input = [9]byte{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	fmt.Println(tester(input))
+}
